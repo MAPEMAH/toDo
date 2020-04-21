24623ac60f42073dfa8c8beee4dca393a9f44c09
MAPEMAH <yagodzinskii.93@gmail.com>
2020-04-22 01:28:15 +0300

    новый .js с примерными новыми объектами

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let money = prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
 
+//создание переменных money and time с вопросами
+let money = prompt('Ваш бюджет на месяц?'); 
+let timeData = prompt('Введите дату в формате YYYY-MM-DD');
+
+//как сделать вопросы не имею понятия и соеденить их по примеру
 let a = prompt('Введите обязательную статью расходов в этом месяце');
 let b = prompt('Во сколько обойдется?');
 let expenses = {a:b};
+//appData обьект со свойствами внутри
 let appData = {
         money, 
-        time,
+        timeData,
         expenses: {},
         optionalExpenses: {},
         income: [],
         savings: false,
     };
 
+    expenses = {
+        let a = 'first';
+    };
 
+    // let x = 'first';
+    // let y = 'second';
+    // let obj = {};
+    // obj[a] = b;
+    // console.log('obj');
 
 console.log(appData);
