3f0c6e9da16a86c45315c9dce7352b442951e33d
валик <valik@valiks-MacBook-Pro.local>
2020-04-22 16:52:58 +0300

    expenses object done

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 
 //создание переменных money and time с вопросами
 let money = prompt('Ваш бюджет на месяц?'); 
-let timeData = prompt('Введите дату в формате YYYY-MM-DD');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
 
-//как сделать вопросы не имею понятия и соеденить их по примеру
-let a = prompt('Введите обязательную статью расходов в этом месяце');
-let b = prompt('Во сколько обойдется?');
-let expenses = {a:b};
+
 //appData обьект со свойствами внутри
 let appData = {
-        money, 
-        timeData,
+        budget: money, 
+        timeData: time,
         expenses: {},
         optionalExpenses: {},
         income: [],
         savings: false,
     };
+    
 
-    expenses = {
-        let a = 'first';
-    };
+    //как сделать вопросы не имею понятия и соеденить их по примеру
+let a = prompt('Введите обязательную статью расходов в этом месяце');
+let b = prompt('Во сколько обойдется?');
+appData.expenses={[a]: b};
+    // expenses = {
+    //     let a = 'first';
+    // };
 
     // let x = 'first';
     // let y = 'second';
