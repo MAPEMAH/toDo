eb6cc21050f5c0df3be5529799f1ce4bfcd71fbe
Sergei <yagodzinskii.93@gmail.com>
2020-04-30 03:18:17 +0300

    almost ready first part

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
+let money = +prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD"); 
 
-//создание переменных money and time с вопросами
-let money = prompt('Ваш бюджет на месяц?'); 
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+ 
 
 
-//appData обьект со свойствами внутри
+
 let appData = {
-        budget: money, 
-        timeData: time,
-        expenses: {},
-        optionalExpenses: {},
-        income: [],
-        savings: false,
-    };
-    
+    budget: money,
+    timeData: time,
+    expenses: {
+        
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+//цикл на 2 вопроса и запись в свойства обьекта expenses
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов", ''),
+        b = prompt("Во сколько обойдется?", '');
+        appData.expenses[a] = b;
+}
+//расчет бюджета на 1 день и запись moneyPerDay в обьект (другим способом1)
+appData.moneyPerDay = appData.budget / 30;
 
-    //как сделать вопросы не имею понятия и соеденить их по примеру
-let a = prompt('Введите обязательную статью расходов в этом месяце');
-let b = prompt('Во сколько обойдется?');
-appData.expenses={[a]: b};
-    // expenses = {
-    //     let a = 'first';
-    // };
+console.log(`Ежедневный бюджет: ${appData.moneyPerDay}`);
+console.log(appData, money, time); //просто вывод в консоль для проверки этих данных
 
-    // let x = 'first';
-    // let y = 'second';
-    // let obj = {};
-    // obj[a] = b;
-    // console.log('obj');
 
-console.log(appData);
+
